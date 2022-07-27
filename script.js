65681500cfc84bda97419c28aadbc3ccd06262e7
WeirdoMeido <meiren8adiaz@gmail.com>
2022-07-27 18:26:08 -0500

    Added functionality for the ereaser button

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let canvas = document.querySelector('.container');
+let color = "black";
 
 for (let i = 0; i < 256; i++) {
     let canDiv = document.createElement('div');
     canDiv.id = 'r' + i;
     canDiv.className = 'pixelDiv';
-    canDiv.addEventListener('mouseover', () => canDiv.style.backgroundColor = "black");
+    canDiv.addEventListener('mouseover', colorBrush);
     canvas.appendChild(canDiv);
 }
 
@@ -14,3 +15,10 @@ function ereaseCanvas() {
     blankCanvas.forEach((div) => div.style.backgroundColor = "white");
 }
 
+function colorBrush() {
+    this.style.backgroundColor = color
+}
+
+function changeBrush(currentColor) {
+ color = currentColor;
+}
