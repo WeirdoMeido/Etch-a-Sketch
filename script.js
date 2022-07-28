1668a235fc22c0a1e93e2418d0eeb7309a87aa17
WeirdoMeido <meiren8adiaz@gmail.com>
2022-07-28 12:52:05 -0500

    Added input for resizable canvas; all functionalities completed

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
 let canvas = document.querySelector('.container');
 let color = "black";
 
-for (let i = 0; i < 256; i++) {
-    let canDiv = document.createElement('div');
-    canDiv.id = 'r' + i;
-    canDiv.className = 'pixelDiv';
-    canDiv.addEventListener('mouseover', colorBrush);
-    canvas.appendChild(canDiv);
+
+function fillCanvas(size) {
+    let canvas = document.querySelector('.container');
+    let squares = canvas.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    canvas.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    canvas.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    let amount = size * size
+
+    for (let i = 0; i < amount; i++) {
+        let canDiv = document.createElement('div');
+        canDiv.id = 'r' + i;
+        canDiv.className = 'pixelDiv';
+        canDiv.style.border = '1px dotted grey'
+        canDiv.addEventListener('mouseover', colorBrush);
+        canvas.appendChild(canDiv);
+    }
 }
+
+fillCanvas(16);
+
+
+function resizeCanvas(input) {
+let canvasSize = parseInt(input);
+    if (canvasSize < 2) {
+        let alertMessage = document.querySelector('.warning');
+        alertMessage.textContent = 'Introduce a number greater than 2';
+    } else if (canvasSize > 100) {
+        let alertMessage = document.querySelector('.warning');
+        alertMessage.textContent = 'Introduce a number smaller than 100';   
+    } else if (typeof(canvasSize) !== 'number' || canvasSize === NaN){
+        let alertMessage = document.querySelector('.warning');
+        alertMessage.textContent = 'Thats an invalid number'; 
+    }
+        else {
+            let alertMessage = document.querySelector('.warning');
+            alertMessage.textContent = ''; 
+            fillCanvas(canvasSize);
+    }
+}
+
+
+/// BRUSH FUNCTIONS///
 
 function ereaseCanvas() {
     let blankCanvas = document.querySelectorAll('.pixelDiv');
